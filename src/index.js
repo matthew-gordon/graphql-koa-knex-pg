de1ed601744b1a10cb4d54c8ed0028a3663b5e03
matthew-gordon <m.gordon5525@gmail.com>
2017-11-20 14:17:02 -0700

    Add graphql boilerplate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 const router = new Router()
 const PORT = process.env.PORT || 3000
 
-router.get('/', async (ctx) => {
-  ctx.body = 'Hello World'
-})
+const routes = require('./routes')
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(bodyParser())
+app.use(routes.routes())
+app.use(routes.allowedMethods())
 
 const server = app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Listening on port ${PORT}...`)
 })
---
+++ src/routes/graphql-router.js
@@ -0,0 +1,11 @@
+const Router = require('koa-router')
+const router = new Router()
+const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
+const GraphQLSchema = require('../schema/schema')
+
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/api/graphql' }));
+router.post('/graphql', graphqlKoa({
+  schema: GraphQLSchema
+}))
+
+module.exports = router.routes()
